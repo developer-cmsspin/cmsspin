c289ff935e33cd8ab0e0eba151833c00a999a2d6
Andres Mora <andres.mora@dracobots.com>
2019-06-24 16:38:02 -0500

    Base solution
    solution base by used CMS SPIN

---
+++ Spin.WebSite/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Spin.Base.Helper.Base;
+
+namespace Spin.WebSite
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //TODO:LIST Provider
+
+            SpinProgram Host = new SpinProgram();
+            Host.CreateWebHost<Startup>().Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
---
+++ Spin.WebSite/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Spin.Base.Company.Entity;
+using Spin.Base.Helper.Base;
+using Spin.Geolocation.MaxMind.API;
+
+
+namespace Spin.WebSite
+{
+    /// <summary>
+    /// Startup.
+    /// </summary>
+    public class Startup : SpinStartup
+    {
+
+        #region Startup
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Spin.WebSite.Startup"/> class.
+        /// </summary>
+        /// <param name="configuration">Configuration.</param>
+        public Startup(IConfiguration configuration)
+            : base(configuration)
+        {
+            
+
+        }
+        #endregion
+    }
+
+}
